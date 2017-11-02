e73ef12de3f79de1a2fbcb1ef29b6dbb2687b3f7
kungkk <kungkk77@gmail.com>
2017-11-02 14:36:31 +0800

    add /articles api

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Http\Request;
+
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::all();
+        return $articles->toJson();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // on angular front end component's view 
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = [
+            'name' => $request->name,
+            'description' => $request->description
+        ];
+        
+        if (Article::create($data)) {
+            return 'article_created';
+        } 
+        else {
+            return 'article_failed_to_created';
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data = Article::find($id);
+        return $data->toJson();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = Article::find($id);
+        $data->name = $request->name;
+        $data->description = $request->description;
+        
+        //if (Article::update($data)) {
+        if ($data->save()) {
+            return 'article_updated';
+        }
+        else {
+            return 'article_failed_to_updated';
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = Article::find($id);
+        if ($data->delete()) {
+            return 'article_delete';
+        }
+        else {
+            return 'article_failed_to_deleted';
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Route::get('/articles', 'ArticleController@index');
+Route::middleware('auth:api')->get('/articles', 'ArticleController@index');
